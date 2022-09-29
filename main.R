a8a3091539fe66174b71c857fe9f8d503b620057
john cardona <johncardona1992@gmail.com>
2022-09-29 16:16:45 +0000

    data frames

--- main.R
+++ main.R
@@ -8,12 +8,12 @@ typeof(x)
 x<-c(1+0i,2+1i)
 typeof(x) 
 
-#vector
+# vector
 x<-vector("numeric", length = 10)
 typeof(x) 
 x
 
-#coersion
+# coersion
 x<-0:10
 x
 class(x)
@@ -22,24 +22,46 @@ as.logical(x)
 as.character(x)
 as.complex(x)
 
-#list
+# list
 x<-list("a",1,2+0i)
 x
 
-#Matrix
+# Matrix
 
 x<-matrix(data=0:5,nrow = 2,ncol = 3)
 x
 dim(x)
 attributes(x)
 
-#convert vector to matrix
+# convert vector to matrix
 x<-0:5
 x
 m<-matrix(data=x,nrow = 2, ncol = 3)
 m
+
 # bind vectors
 x<-1:3
 y<-10:12
 cbind(x,y)
 rbind(x,y)
+
+# factor
+x<-factor(c("YES","NO","NO","YES"))
+x
+table(x)
+unclass(x)
+# set levels
+x<-factor(c("YES","NO","NO","YES"), levels = c("YES","NO"))
+x
+unclass(x)
+
+# missing values
+x<-c(1,NA,NaN,2)
+is.nan(x)
+is.na(x)
+
+# dataframe
+df<-data.frame(nombre = c("a","b","c"), edad = c(23,34,32))
+df
+nrow(df)
+ncol(df)
